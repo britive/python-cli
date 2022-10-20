dd03684ffff869ff1690e85b69cd5d38c6215808
Thomas Rawley <thomas.rawley@britive.com>
2022-10-20 14:05:36 -0400

    removing unneeded files which are for local testing

--- server.py
+++
@@ -1,6 +0,0 @@
-import http.server
-import ssl
-
-httpd = http.server.HTTPServer(('127.0.0.1', 8043), http.server.SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./server.pem', server_side=True)
-httpd.serve_forever()
