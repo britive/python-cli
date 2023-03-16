269a0d7db7eada36ae26839e503bf9665a5932f7
Thomas Rawley <thomas.rawley@britive.com>
2023-03-16 15:51:01 -0400

    fix checkin silent bug and associated test

--- src/pybritive/commands/checkin.py
+++ src/pybritive/commands/checkin.py
@@ -8,7 +8,7 @@ from ..helpers.profile_argument_dectorator import click_smart_profile_argument
 @build_britive
 @britive_options(names='tenant,token,silent,passphrase,federation_provider')
 @click_smart_profile_argument
-def checkin(ctx, tenant, token, passphrase, federation_provider, profile):
+def checkin(ctx, tenant, token, silent, passphrase, federation_provider, profile):
     """Checkin a profile.
 
     This command takes 1 required argument `PROFILE`. This should be a string representation of the profile
