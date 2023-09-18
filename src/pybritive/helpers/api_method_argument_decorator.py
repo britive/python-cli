afb829428f69a65e8007916ca4c7ff544f06f9a8
Thomas Rawley <thomas.rawley@britive.com>
2023-09-18 09:28:33 -0400

    changelog date and conditional shell completion for api

--- src/pybritive/helpers/api_method_argument_decorator.py
+++ src/pybritive/helpers/api_method_argument_decorator.py
@@ -1,12 +1,13 @@
 import click
 import pkg_resources
-from ..completers.api import api_completer
+
 
 click_major_version = int(pkg_resources.get_distribution('click').version.split('.')[0])
 
 
 def click_smart_api_method_argument(func):
     if click_major_version >= 8:
+        from ..completers.api import api_completer
         dec = click.argument('method', shell_complete=api_completer)
     else:
         dec = click.argument('method')
