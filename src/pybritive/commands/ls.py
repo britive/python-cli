eae4c2cd9a5d6f361d47c1ab80888d414cc5ba55
theborch <theborch@gmail.com>
2025-10-16 12:00:14 -0500

    feature:added search_text param to applicable list_* calls

--- src/pybritive/commands/ls.py
+++ src/pybritive/commands/ls.py
@@ -12,34 +12,10 @@ def ls():
 
 @ls.command()
 @build_britive
-@britive_options(names='format,tenant,token,silent,passphrase,federation_provider')
-def applications(ctx, output_format, tenant, token, silent, passphrase, federation_provider):
+@britive_options(names='search_text,format,tenant,token,silent,passphrase,federation_provider')
+def applications(ctx, search_text, output_format, tenant, token, silent, passphrase, federation_provider):
     """List applications for the currently authenticated identity."""
-    ctx.obj.britive.list_applications()
-
-
-@ls.command()
-@build_britive
-@britive_options(names='format,tenant,token,silent,passphrase,federation_provider')
-def environments(ctx, output_format, tenant, token, silent, passphrase, federation_provider):
-    """List environments for the currently authenticated identity."""
-    ctx.obj.britive.list_environments()
-
-
-@ls.command()
-@build_britive
-@britive_options(names='checked_out,profile_type,output_format,tenant,token,silent,passphrase,federation_provider')
-def profiles(ctx, checked_out, profile_type, output_format, tenant, token, silent, passphrase, federation_provider):
-    """List profiles for the currently authenticated identity."""
-    ctx.obj.britive.list_profiles(checked_out=checked_out, profile_type=profile_type)
-
-
-@ls.command()
-@build_britive
-@britive_options(names='format,tenant,token,silent,passphrase,federation_provider')
-def secrets(ctx, output_format, tenant, token, silent, passphrase, federation_provider):
-    """List secrets for the currently authenticated identity."""
-    ctx.obj.britive.list_secrets()
+    ctx.obj.britive.list_applications(search_text=search_text)
 
 
 @ls.command()
@@ -52,7 +28,49 @@ def approvals(ctx, output_format, tenant, token, silent, passphrase, federation_
 
 @ls.command()
 @build_britive
+@britive_options(names='search_text,format,tenant,token,silent,passphrase,federation_provider')
+def environments(ctx, search_text, output_format, tenant, token, silent, passphrase, federation_provider):
+    """List environments for the currently authenticated identity."""
+    ctx.obj.britive.list_environments(search_text=search_text)
+
+
+@ls.command()
+@build_britive
+@britive_options(
+    names='checked_out,profile_type,search_text,output_format,tenant,token,silent,passphrase,federation_provider'
+)
+def profiles(
+    ctx,
+    checked_out,
+    profile_type,
+    search_text,
+    output_format,
+    tenant,
+    token,
+    silent,
+    passphrase,
+    federation_provider,
+):
+    """List profiles for the currently authenticated identity."""
+    ctx.obj.britive.list_profiles(checked_out=checked_out, profile_type=profile_type, search_text=search_text)
+
+
+@ls.command()
+@build_britive
 @britive_options(names='format,tenant,token,silent,passphrase,federation_provider')
-def resources(ctx, output_format, tenant, token, silent, passphrase, federation_provider):
+
+
+@ls.command()
+@build_britive
+@britive_options(names='search_text,format,tenant,token,silent,passphrase,federation_provider')
+def resources(ctx, search_text, output_format, tenant, token, silent, passphrase, federation_provider):
     """List resources for the currently authenticated identity."""
-    ctx.obj.britive.list_resources()
+    ctx.obj.britive.list_resources(search_text=search_text)
+
+
+@ls.command()
+@build_britive
+@britive_options(names='search_text,format,tenant,token,silent,passphrase,federation_provider')
+def secrets(ctx, search_text, output_format, tenant, token, silent, passphrase, federation_provider):
+    """List secrets for the currently authenticated identity."""
+    ctx.obj.britive.list_secrets(search_text=search_text)
