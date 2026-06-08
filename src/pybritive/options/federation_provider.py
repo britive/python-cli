6b4102c6da167bc84629ae766b94f8a0d04f3f49
Samuel Wan <sam.wan@britive.com>
2026-06-08 11:36:08 -0700

    feature: awsstsjwt federation provider support and SDK v4.6.0

--- src/pybritive/options/federation_provider.py
+++ src/pybritive/options/federation_provider.py
@@ -4,7 +4,8 @@ option = click.option(
     '--federation-provider',
     '-P',
     help='Use a federation provider available in the Britive Python SDK for auto token creation. '
-    'See CLI documentation at https://britive.github.io/python-cli/ for acceptable values.',
+    'Valid providers: aws, awsstsjwt, azuresmi, azureumi, bitbucket, gcp, github, gitlab, spacelift. '
+    'See CLI documentation at https://britive.github.io/python-cli/ for details.',
     default=None,
     show_default=True,
 )
